70a31c72b814101bd24336b3ade4c8590dd4a66a
shangyukang <shangyukang@inspur.com>
2020-05-06 10:21:08 +0800

    修改代码：简介里，代码部分markdown 高亮展示代码

--- src/views/overview/Overview.vue
+++ src/views/overview/Overview.vue
@@ -5,7 +5,7 @@
     <b>CSS 弹性盒子布局</b>是 CSS 的模块之一，定义了一种针对用户界面设计而优化的 CSS 盒子模型。在弹性布局模型中，弹性容器的子元素可以在任何方向上排布，也可以“弹性伸缩”其尺寸，既可以增加尺寸以填满未使用的空间，也可以收缩尺寸以避免父元素溢出。子元素的水平对齐和垂直对齐都能很方便的进行操控。通过嵌套这些框（水平框在垂直框内，或垂直框在水平框内）可以在两个维度上构建布局。
   </div>
   <h2>例子</h2>
-  <el-button type="primary" @click="changeFlexFlag">{{"切换弹性元素"}}</el-button>
+  <el-button type="primary" @click="changeFlexFlag">{{"切换弹性元素: " + flg}}</el-button>
   <el-button type="primary" @click="changeDiv">{{"当前为: " + isDiv + ""}}</el-button>
   <br/>
   <br/>
@@ -19,8 +19,14 @@
   </div>
   <h2>对应样式</h2>
   <!--css 样式-->
-  <div class="css-container"  v-if="flag">
+  <div class="css-container" v-show="!flg">
     <span v-for="item in cssContent" :key="item.name" class="css-item">
+      <h1>{{item.name}}</h1>
+      <vue-markdown v-highlight>{{item.cssContent}}</vue-markdown>
+    </span>
+  </div>
+  <div class="css-container" v-show="flg">
+    <span v-for="item in cssContent1" :key="item.name" class="css-item">
       <h1>{{item.name}}</h1>
       <vue-markdown v-highlight>{{item.cssContent}}</vue-markdown>
     </span>
@@ -36,17 +42,18 @@ export default {
   data () {
     return {
       flexFlag: "",
-      flag: true,
+      flg: true,
       isDiv: "div",
       cssContent: [],
+      cssContent1: [],
       isCollapse: true
     };
   },
   mounted () {
+    this.flg = false;
     this.cssContent = [{
       name: "容器",
-      // cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n```"
-      cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n.flex {\ndisplay: flex;\n}\n ```"
+      cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n```"
     }, {
       name: "弹性元素1",
       cssContent: "```css \n.item {\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
@@ -56,6 +63,19 @@ export default {
     }, {
       name: "弹性元素3",
       cssContent: "```css \n.item {\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
+    }];
+    this.cssContent1 = [{
+      name: "容器",
+      cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n.flex {\n  display: flex;\n}\n ```"
+    }, {
+      name: "弹性元素1",
+      cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
+    }, {
+      name: "弹性元素2",
+      cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
+    }, {
+      name: "弹性元素3",
+      cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
     }];
   },
   methods: {
@@ -67,40 +87,14 @@ export default {
       }
     },
     changeFlexFlag () {
-      this.flag = false;
       if (!this.flexFlag) {
         this.flexFlag = " flex";
-        this.cssContent = [{
-          name: "容器",
-          cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n.flex {\ndisplay: flex;\n}\n ```"
-        }, {
-          name: "弹性元素1",
-          cssContent: "```css \n.item {\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }, {
-          name: "弹性元素2",
-          cssContent: "```css \n.item {\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }, {
-          name: "弹性元素3",
-          cssContent: "```css \n.item {\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }];
+        this.flg = true;
       } else {
         this.flexFlag = "";
-        this.cssContent = [{
-          name: "容器",
-          cssContent: "```css \n.container {\n  width: 1200px;\n  height: 400px;\n  background: yellow;\n}\n```"
-        }, {
-          name: "弹性元素1",
-          cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }, {
-          name: "弹性元素2",
-          cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }, {
-          name: "弹性元素3",
-          cssContent: "```css \n.item\n  border: dashed 1px;\n  flex: 1;\n}\n ```"
-        }];
+        this.flg = false;
       }
-      this.flag = true;
-      console.log(this.cssContent);
+      console.log(this.flg);
     }
   },
   components: {
